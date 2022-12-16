41113cd6bae2875a1c9bd42bca865f747e68066b
LucasPCotrim <lucaspcotrim@gmail.com>
2022-12-16 01:52:31 -0300

    feat: implemented HotelPage header

---
+++ src/components/Hotel/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default function HotelPage() {
+  return (
+    <>
+      <PageHeader>Escolha de hotel e quarto</PageHeader>
+    </>
+  );
+}
+
+const PageHeader = styled.div`
+  font-size: 34px;
+  margin-bottom: 36px;
+`;
