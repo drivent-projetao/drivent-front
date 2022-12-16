42a44b34456d9292b9129ef9b334e4f0b9d00761
LucasPCotrim <lucaspcotrim@gmail.com>
2022-12-16 04:00:31 -0300

    feat: implemented functionality to select hotel in ChooseHotelMenu component

--- src/components/Hotel/ChooseHotelMenu.js
+++ src/components/Hotel/ChooseHotelMenu.js
@@ -1,24 +1,35 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import useHotels from '../../hooks/api/useHotels';
 
-function Hotel({ id, name, image }) {
+function Hotel({ id, name, image, selected, handleSelectHotel }) {
   return (
-    <div className="hotel">
+    <HotelContainer selected={selected} onClick={handleSelectHotel}>
       <img src={image} alt="hotel" />
       <div className="hotel-name">{name}</div>
-    </div>
+    </HotelContainer>
   );
 }
 
 export default function ChooseHotelMenu() {
   const { hotels } = useHotels();
+  const [selectedHotel, setSelectedHotel] = useState(0);
 
   return (
     <>
       <MenuHeader>Primeiro, escolha seu hotel</MenuHeader>
       <HotelBrowser>
         {hotels ? (
-          hotels.map((hotel, index) => <Hotel key={index} id={hotel.id} name={hotel.name} image={hotel.image} />)
+          hotels.map((hotel, index) => (
+            <Hotel
+              key={index}
+              id={hotel.id}
+              name={hotel.name}
+              image={hotel.image}
+              selected={hotel.id === selectedHotel}
+              handleSelectHotel={() => setSelectedHotel(hotel.id)}
+            />
+          ))
         ) : (
           <></>
         )}
@@ -39,29 +50,30 @@ const HotelBrowser = styled.div`
   gap: 19px;
   align-items: center;
   justify-content: flex-start;
+`;
 
-  .hotel {
-    background-color: #ebebeb;
-    border-radius: 10px;
-    width: 196px;
-    height: 264px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 14px;
-    cursor: pointer;
-    &:hover {
-      background: linear-gradient(to right, rgba(250, 64, 152, 0.5), rgba(255, 215, 127, 0.5));
-      transform: scale(1.02);
-    }
-    img {
-      width: 168px;
-      height: 109px;
-      border-radius: 5px;
-      margin-bottom: 10px;
-    }
-    .hotel-name {
-      font-size: 20px;
-    }
+const HotelContainer = styled.div`
+  background-color: #ebebeb;
+  border-radius: 10px;
+  width: 196px;
+  height: 264px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 14px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => (props.selected ? '#FFEED2' : '#ccc')};
+    transform: scale(1.02);
+  }
+  background-color: ${(props) => (props.selected ? '#FFEED2' : '#ebebeb')};
+  img {
+    width: 168px;
+    height: 109px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+  }
+  .hotel-name {
+    font-size: 20px;
   }
 `;
